35e06f0fda8eee6a40bd3c99bbe4bfbaf561ab33
sendowyehert01 <senduo.galang0149@gmail.com>
2023-08-08 14:07:11 +0800

    Change page title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Blog App by Rosendo Galang</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
